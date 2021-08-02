f06b9cfe80d97a906da1da91b628d6ac12512933
Pavel Malyshev <CrazyAlarmClock@gmail.com>
2021-08-02 13:44:32 +0300

    fix

--- bpc-cmp/flutter_service_worker.js
+++ bpc-cmp/flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "c8d540cc5f57a76caceeab409bf013fb",
-"/": "c8d540cc5f57a76caceeab409bf013fb",
-"main.dart.js": "0ec79a75667925c66b61a23f6894be64",
+  "index.html": "4dc9e2dd0079819db8056642dff579da",
+"/": "4dc9e2dd0079819db8056642dff579da",
+"main.dart.js": "c825541f945fe3e772304985ff5c9468",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
