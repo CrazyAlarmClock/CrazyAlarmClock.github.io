7f5cac654b4ad9ca98678de9d30b5b901eff66fd
Malyshev <CrazyAlarmClock@gmail.com>
2021-12-06 13:49:11 +0300

    fix

--- ZB/app/themes/chistoapp/dist/scripts/scripts.js
+++
@@ -1,1 +0,0 @@
-No Content: app/themes/chistoapp/dist/scripts/scripts.js
---
+++ ZB/config.js
@@ -0,0 +1,14 @@
+  function doSomething(){
+   var firebaseConfig = {
+     apiKey: "AIzaSyANjXgZKHAoulsICy678Y0FNzRLDoW-Kxc",
+     authDomain: "mbandroid-978.firebaseapp.com",
+     databaseURL: "https://mbandroid-978.firebaseio.com",
+     projectId: "mbandroid-978",
+     storageBucket: "mbandroid-978.appspot.com",
+     messagingSenderId: "1046197433917",
+     appId: "1:1046197433917:web:281b41234336f3394e1c54"
+   };
+  firebase.initializeApp(firebaseConfig);
+   }
+    doSomething();
+
