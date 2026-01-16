53fa328181d723b689afc5ad82ae19b9466be3f0
Pavel Malyshev <CrazyAlarmClock@gmail.com>
2026-01-16 21:15:05 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // 👇 ДОБАВЬТЕ ЭТУ СТРОКУ (замените на название вашего репозитория)
-      base: '/', 
+      base: './', 
       
       server: {
         port: 3000,
