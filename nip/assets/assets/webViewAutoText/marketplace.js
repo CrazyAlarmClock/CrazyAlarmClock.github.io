01661ecd1c6f4ed2853f9579f3f7b5472a48db84
Pavel Malyshev <CrazyAlarmClock@gmail.com>
2026-01-16 20:39:15 +0300

    clear

--- nip/assets/assets/webViewAutoText/marketplace.js
+++
@@ -1,2 +0,0 @@
-document.getElementById("login-popup").value="9272433110";
-document.getElementById("password-popup").value="123456";
--- nip/assets/assets/webViewAutoText/wing.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("first-name").value="Pavel";
-document.getElementById("last-name").value="Malusheve";
-document.getElementById("contact-num").value="89267651560";
-document.getElementById("account-email").value="user@bpc.com";
-
